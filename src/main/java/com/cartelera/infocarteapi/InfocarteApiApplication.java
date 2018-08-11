2a799e8e6c46f979d0c115046e930fe6c77f7a86
Felipe Arana <felipearana10@hotmail.com>
2018-08-11 20:49:39 -0300

    Inicializando spring

---
+++ src/main/java/com/cartelera/infocarteapi/InfocarteApiApplication.java
@@ -0,0 +1,12 @@
+package com.cartelera.infocarteapi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class InfocarteApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(InfocarteApiApplication.class, args);
+    }
+}
---
+++ src/main/java/com/cartelera/infocarteapi/model/User.java
@@ -0,0 +1,48 @@
+package com.cartelera.infocarteapi.model;
+
+import java.io.Serializable;
+import java.util.List;
+
+import javax.persistence.CascadeType;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+import javax.persistence.Inheritance;
+import javax.persistence.InheritanceType;
+import javax.persistence.JoinColumn;
+import javax.persistence.JoinTable;
+import javax.persistence.ManyToMany;
+import javax.persistence.OneToMany;
+import javax.persistence.OneToOne;
+import javax.persistence.Table;
+
+@Entity
+@Table(name ="user")
+@Inheritance(strategy =InheritanceType.JOINED)
+public class Usuario implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue
+    private int id;
+    private String username;
+    private String password;
+    private String email;
+    private int phone;//en caso que tenga wsp se utilizara esta infomacion
+
+    @OneToMany(mappedBy="post")//un usuario tiene muchas publicaciones
+    private List<Post> publicaciones;
+
+    @OneToMany(mappedBy="billboard")//un usuario crea muchas carteleras
+    private List<Billboard> carteleras;
+
+    @OneToMany(mappedBy="comment")
+    private List<Comment> comentarios;
+
+    @OneToOne(cascade={CascadeType.PERSIST, CascadeType.MERGE})//un usuario tiene uno y solo un tipo de notificacion(facebook,wsp,...)
+    @JoinColumn(name ="notification")
+    private Notification notification;
+
+//    @OneToOne
+//    private Template configuracion;//cada  admin va a tener una configuracion
+
+}
