24f805ae45e19fab5ebc961edc5d8feccc6a9350
Felipe Arana <felipearana10@hotmail.com>
2018-08-18 20:32:27 -0300

    Changing public and private methods

--- src/main/java/com/cartelera/infocarteapi/controllers/HomeController.java
+++ src/main/java/com/cartelera/infocarteapi/controllers/HomeController.java
@@ -1,17 +1,25 @@
 package com.cartelera.infocarteapi.controllers;
 
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class HomeController {
 
-  @GetMapping(value = "/")
+  @RequestMapping("/index")
   public String home(){
-    return "Hola home";
+    return "Hola index";
   }
 
-  @GetMapping(value = "/backend")
-  public String backend(){
-    return "Hola back";
+  @RequestMapping("/admin")
+  public String adminBackend(){
+    return "Hola admin";
+  }
+
+  @RequestMapping("/user")
+  public String userBackend(){
+    return "Hola usuario";
   }
 
 }
