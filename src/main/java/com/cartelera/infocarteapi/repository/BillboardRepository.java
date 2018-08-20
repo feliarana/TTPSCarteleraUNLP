df2480ae6aba0da4a349bceb3dbd93987d14daf5
Felipe Arana <felipe.arana@aero.tur.ar>
2018-08-20 12:11:55 -0300

    findByTitle

--- src/main/java/com/cartelera/infocarteapi/repository/BillboardRepository.java
+++ src/main/java/com/cartelera/infocarteapi/repository/BillboardRepository.java
@@ -1,10 +1,16 @@
 package com.cartelera.infocarteapi.repository;
 
 import org.springframework.data.repository.PagingAndSortingRepository;
+import org.springframework.data.repository.query.Param;
 import org.springframework.data.rest.core.annotation.RepositoryRestResource;
 
 import com.cartelera.infocarteapi.entities.Billboard;
+import org.springframework.web.bind.annotation.CrossOrigin;
 
+import java.util.List;
+
+@CrossOrigin
 @RepositoryRestResource(path = "billboards", collectionResourceRel = "billboards")
 public interface BillboardRepository extends PagingAndSortingRepository<Billboard, Long> {
+  List<Billboard> findByTitle(@Param("title") String title);
 }
