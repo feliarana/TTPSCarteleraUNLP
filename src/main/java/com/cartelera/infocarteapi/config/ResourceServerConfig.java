d8a6cb38de68d7fde77177882d90d66989d94bc1
Felipe Arana <felipe.arana@aero.tur.ar>
2018-09-11 09:36:32 -0300

    cambiando conf cors

--- src/main/java/com/cartelera/infocarteapi/config/ResourceServerConfig.java
+++ src/main/java/com/cartelera/infocarteapi/config/ResourceServerConfig.java
@@ -62,15 +62,5 @@ public class ResourceServerConfig extends ResourceServerConfigurerAdapter {
       .antMatchers("/admin/**","/user/**").authenticated().and().cors();
   }
 
-  @Bean
-  public CorsConfigurationSource corsConfigurationSource() {
-    CorsConfiguration configuration = new CorsConfiguration();
-    configuration.setAllowedOrigins(Arrays.asList("*"));
-    configuration.setAllowedMethods(Arrays.asList("GET", "POST", "PUT", "PATCH", "DELETE", "OPTIONS"));
-    configuration.setAllowedHeaders(Arrays.asList("authorization", "content-type", "x-auth-token"));
-    configuration.setExposedHeaders(Arrays.asList("x-auth-token"));
-    UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
-    source.registerCorsConfiguration("/**", configuration);
-    return source;
-  }
+
 }
