e2b52f0a752439f470ec3bc0a3c4a13ba1f35513
Felipe Arana <felipearana10@hotmail.com>
2018-12-16 11:05:23 -0300

    remodeling

--- src/main/java/com/cartelera/infocarteapi/InfocarteApiApplication.java
+++ src/main/java/com/cartelera/infocarteapi/InfocarteApiApplication.java
@@ -1,63 +1,27 @@
 package com.cartelera.infocarteapi;
 
-import com.cartelera.infocarteapi.model.Billboard;
-import com.cartelera.infocarteapi.model.Role;
-import com.cartelera.infocarteapi.model.User;
-import com.cartelera.infocarteapi.repository.BillboardRepository;
-import com.cartelera.infocarteapi.service.UserDetailsServiceImpl;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.ApplicationArguments;
-import org.springframework.boot.ApplicationRunner;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder;
-import org.springframework.security.crypto.password.PasswordEncoder;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.data.jpa.convert.threeten.Jsr310JpaConverters;
 
-import java.util.Arrays;
+import javax.annotation.PostConstruct;
+import java.util.TimeZone;
 
 @SpringBootApplication
-public class  InfocarteApiApplication {
+@EntityScan(basePackageClasses = {
+  InfocarteApiApplication.class,
+  Jsr310JpaConverters.class
+})
+public class InfocarteApiApplication {
 
-    @Autowired
-    private BillboardRepository billboardRepo;
-
-    @Component
-    class DataSetup implements ApplicationRunner{
-        @Override
-        public void run(ApplicationArguments run) throws Exception{
-          billboardRepo.save(Billboard.builder().title("Cartelera Principal").build());
-          billboardRepo.save(Billboard.builder().title("1ero").build());
-          billboardRepo.save(Billboard.builder().title("2do").build());
-          billboardRepo.save(Billboard.builder().title("3ero").build());
-          billboardRepo.save(Billboard.builder().title("4to").build());
-          billboardRepo.save(Billboard.builder().title("5to").build());
-          billboardRepo.save(Billboard.builder().title("Oportunidades").build());
-          billboardRepo.save(Billboard.builder().title("Ofertas laborales").build());
-
-        }
-    }
-
-//  @Bean
-//  public CommandLineRunner setupDefaultUser(UserDetailsServiceImpl service) {
-//    return args -> {
-//      service.save(new User(
-//        "root", //username
-//        "root", //password
-//        Arrays.asList(new Role("ROLE_USER"), new Role("ROLE_ACTUATOR")),//roles
-//        true//Active
-//      ));
-//    };
-//  }
-
-  @Bean
-  public PasswordEncoder getPasswordEncoder(){
-    return new BCryptPasswordEncoder();
+  @PostConstruct
+  void init() {
+    TimeZone.setDefault(TimeZone.getTimeZone("UTC"));
   }
 
   public static void main(String[] args) {
     SpringApplication.run(InfocarteApiApplication.class, args);
   }
 }
+
---
+++ src/main/java/com/cartelera/infocarteapi/repository/RoleRepository.java
@@ -0,0 +1,12 @@
+package com.cartelera.infocarteapi.repository;
+
+import com.cartelera.infocarteapi.model.Role;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import java.util.Optional;
+
+@Repository
+public interface RoleRepository extends JpaRepository<Role, Long> {
+  Optional<Role> findByName(String roleName);
+}
