3e3203ae27c5b45e0269311017eb6e0734bfd47a
Felipe Arana <felipearana10@hotmail.com>
2019-02-05 19:50:28 -0300

    sql config

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -8,8 +8,8 @@ INSERT INTO users (active, password, username) VALUES (1, '$2a$10$dhqE2Ac0Z.ZKTu
 INSERT INTO users (active, password, username) VALUES (1, '$2a$10$wzqz/gJMaySr4Vb7LOWx8eMQDt57fy5MuGVqaO1r58HNdcmYX/wd6', "profesor");
 -- Establecemos roles
 INSERT INTO users_roles(user_id, role_id) VALUES ('1', '1');
-INSERT INTO users_roles(user_id, role_id) VALUES ('2', '2');
-INSERT INTO users_roles(user_id, role_id) VALUES ('3', '3');
+INSERT INTO users_roles(user_id, role_id) VALUES ('11', '11');
+INSERT INTO users_roles(user_id, role_id) VALUES ('21', '21');
 -- Agregamos carteleras
 INSERT INTO billboards(created_at, title, updated_at, user_id) VALUES(CURRENT_TIMESTAMP(), 'Cartelera 1ero', NULL, '1');
 INSERT INTO billboards(created_at, title, updated_at, user_id) VALUES(CURRENT_TIMESTAMP(), 'Cartelera 2do', NULL, '1');
@@ -17,24 +17,24 @@ INSERT INTO billboards(created_at, title, updated_at, user_id) VALUES(CURRENT_TI
 INSERT INTO billboards(created_at, title, updated_at, user_id) VALUES(CURRENT_TIMESTAMP(), 'Cartelera 4to', NULL, '1');
 INSERT INTO billboards(created_at, title, updated_at, user_id) VALUES(CURRENT_TIMESTAMP(), 'Cartelera 5to', NULL, '1');
 -- Agregamos publicaciones
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Horarios 1', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Resultados Adp', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Resultados Arq', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Resultados Org', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('2', 1, CURRENT_TIMESTAMP(), 'Horarios 2', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('2', 1, CURRENT_TIMESTAMP(), 'Resultados Ibd', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('2', 1, CURRENT_TIMESTAMP(), 'Resultados Ing1', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('2', 1, CURRENT_TIMESTAMP(), 'Resultados Ayed', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('2', 1, CURRENT_TIMESTAMP(), 'Horarios 3', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('3', 1, CURRENT_TIMESTAMP(), 'Resultados Mate 3', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('3', 1, CURRENT_TIMESTAMP(), 'Resultados Redes', NULL, '3');
-INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('3', 1, CURRENT_TIMESTAMP(), 'Resultados TTPS Java', NULL, '3');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Horarios 1', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Resultados Adp', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Resultados Arq', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('1', 1, CURRENT_TIMESTAMP(), 'Resultados Org', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('11', 1, CURRENT_TIMESTAMP(), 'Horarios 2', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('11', 1, CURRENT_TIMESTAMP(), 'Resultados Ibd', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('11', 1, CURRENT_TIMESTAMP(), 'Resultados Ing1', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('11', 1, CURRENT_TIMESTAMP(), 'Resultados Ayed', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('21', 1, CURRENT_TIMESTAMP(), 'Horarios 3', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('21', 1, CURRENT_TIMESTAMP(), 'Resultados Mate 3', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('21', 1, CURRENT_TIMESTAMP(), 'Resultados Redes', NULL, '21');
+INSERT INTO posts(billboard_id, comments_enabled, created_at, title, updated_at, user_id) VALUES('21', 1, CURRENT_TIMESTAMP(), 'Resultados TTPS Java', NULL, '21');
 -- Agregamos comentarios
-INSERT INTO comments(content, post_id, user_id) VALUES('Genial! Al fin publican los horarios.', '1', '2');
-INSERT INTO comments(content, post_id, user_id) VALUES('Aprobeeee.', '1', '2');
-INSERT INTO comments(content, post_id, user_id) VALUES('Uh tengo que recursar', '6', '2');
-INSERT INTO comments(content, post_id, user_id) VALUES('APROBAMOS TTPS!!', '12', '2');
+INSERT INTO comments(content, post_id, user_id) VALUES('Genial! Al fin publican los horarios.', '11', '11');
+INSERT INTO comments(content, post_id, user_id) VALUES('Aprobeeee.', '21', '11');
+INSERT INTO comments(content, post_id, user_id) VALUES('Uh tengo que recursar', '21', '11');
+INSERT INTO comments(content, post_id, user_id) VALUES('APROBAMOS TTPS!!', '1', '11');
 -- Agregamos seguidores a carteleras
-INSERT INTO followed_billboards(user_id, billboard_id) VALUES('2', '1');
-INSERT INTO followed_billboards(user_id, billboard_id) VALUES('2', '2');
-INSERT INTO followed_billboards(user_id, billboard_id) VALUES('2', '3');
+INSERT INTO followed_billboards(user_id, billboard_id) VALUES('11', '1');
+INSERT INTO followed_billboards(user_id, billboard_id) VALUES('11', '11');
+INSERT INTO followed_billboards(user_id, billboard_id) VALUES('11', '41');
