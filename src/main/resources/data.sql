30479614f71a199a40fb9bdac6125c51f227eaf0
Emi Retamar <emiliano_retamar@hotmail.com.ar>
2019-03-06 01:37:24 -0300

    Acomodo unas cosas. Igual falta rellenar bien con mas datos.

--- src/main/resources/data.sql
+++ src/main/resources/data.sql
@@ -11,6 +11,7 @@ INSERT INTO public.users_roles(user_id, role_id) VALUES ('1', '1');
 INSERT INTO public.users_roles(user_id, role_id) VALUES ('2', '2');
 INSERT INTO public.users_roles(user_id, role_id) VALUES ('3', '3');
 -- Agregamos carteleras
+INSERT INTO public.billboards(created_at, title, description, image, user_id) VALUES(current_timestamp, 'Cartelera 2do', 'Los newbies', 'https://cdn-images-1.medium.com/max/2000/1*gycg7f5bYLuR4ut_JAEs7A.png', '1');
 INSERT INTO public.billboards(created_at, title, description, image, user_id) VALUES(current_timestamp, 'Cartelera 2do', 'Los walleros', 'https://cdn.pixabay.com/photo/2016/06/18/17/42/image-1465348_960_720.jpg', '1');
 INSERT INTO public.billboards(created_at, title, description, image, user_id) VALUES(current_timestamp, 'Cartelera 3ero', 'Los resistentes', 'https://www.w3schools.com/w3css/img_lights.jpg', '1');
 INSERT INTO public.billboards(created_at, title, description, image, user_id) VALUES(current_timestamp, 'Cartelera 4to', 'Los tranka', 'https://sample-videos.com/img/Sample-jpg-image-500kb.jpg', '1');
@@ -24,17 +25,17 @@ INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description
 INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('2', true, current_timestamp, 'acid', '', 'Resultados Ibd', NULL, '3');
 INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('2', true, current_timestamp, 'muy bien', '', 'Resultados Ing1', NULL, '3');
 INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('2', true, current_timestamp, 'falto t de n', '', 'Resultados Ayed', NULL, '3');
-INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('2', true, current_timestamp, 'agarrate con concu', '', 'Horarios 3', NULL, '3');
-INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('2', true, current_timestamp, 'probabilidades de aprobar', '', 'Resultados Mate 3', NULL, '3');
-INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('2', true, current_timestamp, 'recursada nro 1920194', '', 'Resultados Redes', NULL, '3');
-INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('2', true, current_timestamp, 'los mas copados', 'https://wallpaperbrowse.com/media/images/3848765-wallpaper-images-download.jpg', 'Resultados TTPS Java', NULL, '3');
+INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('3', true, current_timestamp, 'agarrate con concu', '', 'Horarios 3', NULL, '3');
+INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('3', true, current_timestamp, 'probabilidades de aprobar', '', 'Resultados Mate 3', NULL, '3');
+INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('3', true, current_timestamp, 'recursada nro 1920194', '', 'Resultados Redes', NULL, '3');
+INSERT INTO public.posts(billboard_id, comments_enabled, created_at, description, image, title, updated_at, user_id) VALUES('3', true, current_timestamp, 'los mas copados', 'https://i0.wp.com/thehappening.com/wp-content/uploads/2016/12/perro-1.jpg?resize=620%2C420&ssl=1', 'Resultados TTPS Java', NULL, '3');
 -- Agregamos comentarios
 INSERT INTO public.comments(comment, post_id, user_id, created_at) VALUES('Genial! Al fin publican los horarios.', '1', '3', current_timestamp);
 INSERT INTO public.comments(comment, post_id, user_id, created_at) VALUES('Aprobeeee.', '2', '3', current_timestamp);
 INSERT INTO public.comments(comment, post_id, user_id, created_at) VALUES('Uh tengo que recursar', '2', '3', current_timestamp);
 INSERT INTO public.comments(comment, post_id, user_id, created_at) VALUES('APROBAMOS TTPS!!', '1', '3', current_timestamp);
 -- Agregamos seguidores a carteleras
-INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('1', '1');
-INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('1', '2');
-INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('1', '3');
-INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('1', '4');
+INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('2', '1');
+INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('2', '2');
+INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('2', '3');
+INSERT INTO public.followed_billboards(user_id, billboard_id) VALUES('2', '4');
