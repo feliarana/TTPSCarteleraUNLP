4ccca4d63b97861518d089af8d330128d89b4d83
Felipe Arana <felipearana10@hotmail.com>
2019-03-17 17:17:34 -0300

    Permission for authorization

--- src/main/java/com/cartelera/infocarteapi/config/SecurityConfig.java
+++ src/main/java/com/cartelera/infocarteapi/config/SecurityConfig.java
@@ -139,7 +139,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     configuration.setAllowCredentials(true);
     // setAllowedHeaders is important! Without it, OPTIONS preflight request
     // will fail with 403 Invalid CORS request
-    configuration.setAllowedHeaders(Arrays.asList("Permission", "Cache-Control", "Content-Type"));
+    configuration.setAllowedHeaders(Arrays.asList("Authorization", "Cache-Control", "Content-Type"));
     final UrlBasedCorsConfigurationSource source = new UrlBasedCorsConfigurationSource();
     source.registerCorsConfiguration("/**", configuration);
     return source;
--- src/main/java/com/cartelera/infocarteapi/config/WebMvcConfig.java
+++ src/main/java/com/cartelera/infocarteapi/config/WebMvcConfig.java
@@ -12,10 +12,6 @@ public class WebMvcConfig implements WebMvcConfigurer {
     registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE", "HEAD", "OPTIONS")
       .allowedOrigins("*")
       .allowedHeaders("*");
-    //    registry.addMapping("/**").allowedMethods("GET", "POST", "DELETE", "HEAD", "OPTIONS")
-//      .allowedOrigins("*")
-//      .allowedHeaders("Permission", "Cache-Control", "Content-Type", "Accept", "X-Requested-With", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin")
-//      .exposedHeaders("Access-Control-Expose-Headers", "Permission", "Cache-Control", "Content-Type", "Access-Control-Allow-Origin", "Access-Control-Allow-Headers", "Origin");
   }
 
 
